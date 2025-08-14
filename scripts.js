a50f4441736552aad0481d5f58137995c2f9f4e3
Vinicius <vinicius.paiva2@outlook.com>
2025-08-14 11:09:27 -0300

    Site adicionado

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('.hamburguer-menu').on('click', function () {
+    $('.toggle').toggleClass('open');
+    $('.nav-list').toggleClass('open');
+  });
+
+  AOS.init({
+    easing: 'ease',
+    duration: 1000,
+  });
+});
+
